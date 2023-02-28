2fcb2468a0691c1f832ed0b5a68e7398683cb629
Selin Butun <sbutun1@goldengate.cs.brynmawr.edu>
2023-02-27 21:23:06 -0500

    complete

--- A06/bitmap.c
+++ A06/bitmap.c
@@ -1,7 +1,7 @@
-/*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+/*
+* Date: March 1, 2023
+ * Description: Program that reads in a file containing
+ * a hex code, and prints out a 8x8 1 bpp sprite.
  ---------------------------------------------*/
 #include <stdio.h>
 
@@ -9,7 +9,22 @@ int main() {
   unsigned long img;
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
-
-  // todo: your code here
+  long bitmask;
+  //print 2d array
+  bitmask = 0x1ul << 63;
+  for(int i = 8; i > 0; i--){
+    for(int j = 8; j > 0; j--){
+        if((img & bitmask) == bitmask){
+            printf("@ ");
+        }
+        else{
+            printf("  ");
+        }
+	img = img << 1;
+    }
+    printf("\n");
+  }
+  printf("\n---\n");
   return 0;
+
 }
