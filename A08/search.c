28bceca76e828a5e0c7c4def8c9666030717bb9b
Selin Butun <sbutun1@goldengate.cs.brynmawr.edu>
2023-04-26 18:05:54 -0400

    assignment complete

--- A08/search.c
+++ A08/search.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void search(int * array, int begin, int end, int input, int pid, int *value){
+  for(int i = begin; i < end; i++){
+    if(array[i] == input){
+      if(pid == 0){
+        printf("Child found %d at index %d\n", input, i);
+        *value = 50;
+        break;
+      }
+      else{
+        printf("Parent found %d at index %d\n", input, i);
+        *value = 50;
+        break;
+      }
+    }
+    else{
+      *value = 0;
+    }
+  }
+  
+}
 
 int main() {
-  return 0;
+  FILE* fp = fopen("data.bin", "rb");
+  int num;
+  fread(&num, sizeof(int), 1, fp);
+  int *array = malloc(num * sizeof(int));
+  fread(array, sizeof(int), num, fp);
+  int input;
+  printf("Enter a value to search: ");
+  scanf("%d", &input);
+  int value = 900;
+
+  int pid = fork();
+
+  for(int i = 0; i < 1; i++){
+    if(pid == 0){
+      search(array, 0, (num/2), input, pid, &value);
+    }
+    else{
+      search(array, (num/2), num, input, pid, &value);
+    }
+  }
+  if(value == 0){
+    printf("not found\n");
+  }
+
+  fclose(fp); 
+  free(array);
 }
